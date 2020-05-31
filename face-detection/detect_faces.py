82ab5da171e3e543bff2f7acbf2d1400f04e0103
Ebuka Achonwa <Achonwaechris@outlook.com>
2020-05-31 22:58:51 +0100

    Update
    updated files

--- face-detection/detect_faces.py
+++ face-detection/detect_faces.py
@@ -8,6 +8,11 @@ import argparse
 import cv2
 
 # construct the argument parse and parse the arguments
+# construct the argument parse and parse the arguments
+# construct the argument parse and parse the arguments
+# construct the argument parse and parse the arguments
+
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
