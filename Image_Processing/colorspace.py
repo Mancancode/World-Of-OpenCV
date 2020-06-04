e0860882294b26ff9e7394b423a88717730527fe
Ebuka Achonwa <Achonwaechris@outlook.com>
2020-06-04 22:29:37 +0100

    update
    update

--- Image_Processing/colorspace.py
+++ Image_Processing/colorspace.py
@@ -1,12 +1,20 @@
 #import needed libraries
+#import needed libraries
+#import needed libraries
 import numpy as np
 import sys
 import imutils
 import argparse
 import cv2
 
-image = cv2.imread("image/jaw.jpg")
 
+
+
+image = cv2.imread("image/jaw.jpg")
+                     
+  
+#import needed libraries
+  
 #Color filtering 
 hsv_image = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 cv2.imshow("hsv_image",hsv_image)
